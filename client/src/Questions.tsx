ce508f80d966c06f804cbfa75b3a26ad2a78ed31
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-06-24 18:14:50 +0300

    uncomment questions

--- client/src/Questions.tsx
+++ client/src/Questions.tsx
@@ -25,130 +25,130 @@ export default
     text: "The time I spent using the game just slipped away.",
     dimensionIdentifier: "FA"
 },
-// {
-//     id: 5,
-//     text: "I was absorbed in this experience.",
-//     dimensionIdentifier: "FA"
-// },
-// {
-//     id: 6,
-//     text: "During this experience I let myself go.",
-//     dimensionIdentifier: "FA"
-// },
-// {
-//     id: 7,
-//     text: "I felt frustrated while using this the game.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 8,
-//     text: "I found this the game confusing to use.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 9,
-//     text: "I felt annoyed while using the game.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 10,
-//     text: "I felt discouraged while using this the game.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 11,
-//     text: "Using this the game was taxing.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 12,
-//     text: "This experience was demanding.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 13,
-//     text: "I felt in control while using this the game.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 14,
-//     text: "I could not do some of the things I needed to do while using the game.",
-//     dimensionIdentifier: "PU"
-// },
-// {
-//     id: 15,
-//     text: "This the game was attractive.",
-//     dimensionIdentifier: "AE"
-// },
-// {
-//     id: 16,
-//     text: "This the game was aesthestically appealing.",
-//     dimensionIdentifier: "AE"
-// },
-// {
-//     id: 17,
-//     text: "I liked the graphics and images of the game.",
-//     dimensionIdentifier: "AE"
-// },
-// {
-//     id: 18,
-//     text: "the game appealed to be visual senses.",
-//     dimensionIdentifier: "AE"
-// },
-// {
-//     id: 19,
-//     text: "The screen layout of the game was visually pleasing.",
-//     dimensionIdentifier: "AE"
-// },
-// {
-//     id: 20,
-//     text: "Using the game was worthwhile.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 21,
-//     text: "I consider my experience a success.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 22,
-//     text: "This experience did not work out the way I had planned.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 23,
-//     text: "My experience was rewarding.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 24,
-//     text: "My experience was rewarding.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 25,
-//     text: "I continued to use the game out of curiosity.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 26,
-//     text: "The content of the game incited my curiosity.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 27,
-//     text: "I was really drawn into this experience.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 28,
-//     text: "I felt involved in this experience.",
-//     dimensionIdentifier: "RW"
-// },
-// {
-//     id: 29,
-//     text: "This experience was fun.",
-//     dimensionIdentifier: "RW"
-// },
+{
+    id: 5,
+    text: "I was absorbed in this experience.",
+    dimensionIdentifier: "FA"
+},
+{
+    id: 6,
+    text: "During this experience I let myself go.",
+    dimensionIdentifier: "FA"
+},
+{
+    id: 7,
+    text: "I felt frustrated while using this the game.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 8,
+    text: "I found this the game confusing to use.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 9,
+    text: "I felt annoyed while using the game.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 10,
+    text: "I felt discouraged while using this the game.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 11,
+    text: "Using this the game was taxing.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 12,
+    text: "This experience was demanding.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 13,
+    text: "I felt in control while using this the game.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 14,
+    text: "I could not do some of the things I needed to do while using the game.",
+    dimensionIdentifier: "PU"
+},
+{
+    id: 15,
+    text: "This the game was attractive.",
+    dimensionIdentifier: "AE"
+},
+{
+    id: 16,
+    text: "This the game was aesthestically appealing.",
+    dimensionIdentifier: "AE"
+},
+{
+    id: 17,
+    text: "I liked the graphics and images of the game.",
+    dimensionIdentifier: "AE"
+},
+{
+    id: 18,
+    text: "the game appealed to be visual senses.",
+    dimensionIdentifier: "AE"
+},
+{
+    id: 19,
+    text: "The screen layout of the game was visually pleasing.",
+    dimensionIdentifier: "AE"
+},
+{
+    id: 20,
+    text: "Using the game was worthwhile.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 21,
+    text: "I consider my experience a success.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 22,
+    text: "This experience did not work out the way I had planned.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 23,
+    text: "My experience was rewarding.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 24,
+    text: "My experience was rewarding.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 25,
+    text: "I continued to use the game out of curiosity.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 26,
+    text: "The content of the game incited my curiosity.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 27,
+    text: "I was really drawn into this experience.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 28,
+    text: "I felt involved in this experience.",
+    dimensionIdentifier: "RW"
+},
+{
+    id: 29,
+    text: "This experience was fun.",
+    dimensionIdentifier: "RW"
+},
 ]
 
