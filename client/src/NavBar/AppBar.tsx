24ec8b2200905ef7fc5c22a111c2cdef4d2231fb
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-06-24 16:52:04 +0300

    delete pagination, add next and pre buttons, edit navbar

--- client/src/NavBar/AppBar.tsx
+++ client/src/NavBar/AppBar.tsx
@@ -15,62 +15,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVrCardboard } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
-const pages = [
-    {
-        name: 'Home',
-        path: '/'
-    },
-    {
-        name: 'About us',
-        path: '/'
-    }, 
-];
 
 function ResponsiveAppBar() {
-    const navigate = useNavigate();
-
-    const handleOnClick = (path: string) => {
-        navigate(path);
-    };
-
 
     return (
         <AppBar position="static" style={{backgroundColor:"#3E0758"}}>
-        <Container maxWidth="xl">
-            <Toolbar disableGutters>
-            <FontAwesomeIcon icon={faVrCardboard} />
-            <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="/"
-                sx={{
-                mr: 6,
-                ml: 1,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                color: 'inherit',
-                textDecoration: 'none',
-                }}
-            >
-                VR Rehacom
-            </Typography>
-
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                {pages.map((page) => (
-                    <Button
-                    key={page.name}
-                    onClick={() => handleOnClick(page.path)}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <FontAwesomeIcon icon={faVrCardboard} />
+                    <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                    mr: 2,
+                    ml: 1,
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
                     >
-                        {page.name}
-                    </Button>
-                ))}
-            </Box>
-
-            </Toolbar>
-        </Container>
+                        VR Rehacom
+                    </Typography>
+                </Toolbar>
+            </Container>
         </AppBar>
     );
 }
